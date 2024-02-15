6b3f93bd44de435651db89677b80e8c0f564c755
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-02-15 16:57:26 +0200

    admin index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,7 +16,6 @@
             <a href="{{ route('imagemeta.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded-md">ADD</a>
         </div>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
